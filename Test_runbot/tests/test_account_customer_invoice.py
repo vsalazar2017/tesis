4fe9b14729620f0ae507ba76bab1aadf1ebe89db
victor <victor.salazar@trescloud.com>
2018-09-12 23:52:40 -0500

    Correccion de test

--- Test_runbot/tests/test_account_customer_invoice.py
+++ Test_runbot/tests/test_account_customer_invoice.py
@@ -2,9 +2,56 @@ from odoo.addons.account.tests.account_test_users import AccountTestUsers
 import datetime
 
 
-class TestAccountCustomerInvoice(AccountTestUsers):
+class TestAccountCustomerInvoiceCheck(AccountTestUsers):
 
-    def test_customer_invoice(self):
-        super(TestAccountCustomerInvoice, self).test_customer_invoice()
+    def test_customer_invoiceCheck(self):
+        #super(TestAccountCustomerInvoice, self).test_customer_invoice()
         #Agregando Test de check
-        assert (self.account_invoice_customer0.check == False), "Debe ser Verdadero"
+        self.account_invoice_obj = self.env['account.invoice']
+        self.payment_term = self.env.ref('account.account_payment_term_advance')
+        self.journalrec = self.env['account.journal'].search([('type', '=', 'sale')])[0]
+        self.partner3 = self.env.ref('base.res_partner_3')
+        account_user_type = self.env.ref('account.data_account_type_receivable')
+        self.ova = self.env['account.account'].search([('user_type_id', '=', self.env.ref('account.data_account_type_current_assets').id)], limit=1)
+
+        #only adviser can create an account
+        self.account_rec1_id = self.account_model.sudo(self.account_manager.id).create(dict(
+            code="cust_acc",
+            name="customer account",
+            user_type_id=account_user_type.id,
+            reconcile=True,
+        ))
+
+        invoice_line_data = [
+            (0, 0,
+                {
+                    'product_id': self.env.ref('product.product_product_5').id,
+                    'quantity': 2.0,
+                    'account_id': self.env['account.account'].search([('user_type_id', '=', self.env.ref('account.data_account_type_revenue').id)], limit=1).id,
+                    'name': 'product test 5',
+                    'price_unit': 100.00,
+                }
+             )
+        ]
+
+        self.account_invoice_customer_test = self.account_invoice_obj.sudo(self.account_user.id).create(dict(
+            name="Test Customer Invoice",
+            reference_type="none",
+            payment_term_id=self.payment_term.id,
+            journal_id=self.journalrec.id,
+            partner_id=self.partner3.id,
+            account_id=self.account_rec1_id.id,
+            invoice_line_ids=invoice_line_data
+        ))
+
+        # I manually assign tax on invoice
+        invoice_tax_line = {
+            'name': 'Test Tax for Customer Invoice 0',
+            'manual': 1,
+            'amount': 9050,
+            'account_id': self.ova.id,
+            'invoice_id': self.account_invoice_customer_test.id,
+        }
+        tax = self.env['account.invoice.tax'].create(invoice_tax_line)
+
+        assert (self.account_invoice_customer_test.check == False), "Debe ser Verdadero"
